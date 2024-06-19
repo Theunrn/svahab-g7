e94eb8b96acd231b8a0e283a4da246330069f27d
KOEMSRAN.PHON <KOEMSRAN.PHON@PNC.LAN>
2024-06-19 19:35:10 +0700

    interface-home-page GSA-7

---
+++ frontend/src/Components/Bodies/MatchTeam.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- How It Works -->
+    <div class="py-12 bg-gray-50">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h2 class="text-3xl font-bold text-gray-900 mb-1 text-center">WE ARE FINDING MATCH TEAM FOR PLAYING</h2>
+        <p class="text-center mb-5">Let match with us!</p>
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+          <div class="text-center">
+            <div class="flex items-center justify-center mb-4 text-3xl text-blue-500">
+              <img width="200" height="200" class="border border-2 border-info" src="../../assets/image/match.jpg" alt="">
+            </div>
+            <h3 class="text-xl font-semibold text-gray-900 mb-2">Match</h3>
+            <p class="text-gray-700">Find fields near you by searching with location and date.</p>
+          </div>
+          <div class="text-center">
+            <div class="flex items-center justify-center mb-4 text-3xl text-blue-500">
+                <img width="200" height="200" class="border border-2 border-info" src="../../assets/image/match.jpg" alt="">
+            </div> 
+            <h3 class="text-xl font-semibold text-gray-900 mb-2">Match</h3>
+            <p class="text-gray-700">Choose from a variety of fields that match your needs.</p>
+          </div>
+          <div class="text-center">
+            <div class="flex items-center justify-center mb-4 text-3xl text-blue-500">
+                <img width="200" height="200" class="border border-2 border-info" src="../../assets/image/match.jpg" alt="">
+            </div>
+            <h3 class="text-xl font-semibold text-gray-900 mb-2">Match</h3>
+            <p class="text-gray-700">Book your field easily with our secure online payment system.</p>
+          </div>
+          <div class="text-center">
+            <div class="flex items-center justify-center mb-4 text-3xl text-blue-500">
+                <img width="200" height="200" class="border border-2 border-info" src="../../assets/image/match.jpg" alt="">
+            </div>
+            <h3 class="text-xl font-semibold text-gray-900 mb-2">Match</h3>
+            <p class="text-gray-700">Look and buy sprot material easily with our secure online payment system.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+  
