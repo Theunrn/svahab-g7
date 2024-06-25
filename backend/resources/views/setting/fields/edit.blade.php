68302db391a7306cfa3b10541bd69ede50667dc8
Leak <vorleak.kun@student.passerellesnumeriques.org>
2024-06-25 16:08:41 +0700

    #GSA-25 update interface-field

--- backend/resources/views/setting/fields/edit.blade.php
+++ backend/resources/views/setting/fields/edit.blade.php
@@ -1,55 +1,56 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Edit Field
+        </h2>
+    </x-slot>
 
-@section('content')
-<div class="container">
-    <h2>Edit Field</h2>
+    <div class="container mt-4">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
 
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        <form action="{{ route('admin.fields.update', $field->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="field_name">Field Name</label>
+                <input type="text" class="form-control" id="field_name" name="field_name" value="{{ $field->field_name }}" required>
+            </div>
+            <div class="form-group">
+                <label for="field_location">Field Location</label>
+                <input type="text" class="form-control" id="field_location" name="field_location" value="{{ $field->field_location }}" required>
+            </div>
+            <div class="form-group">
+                <label for="field_type">Field Type</label>
+                <input type="text" class="form-control" id="field_type" name="field_type" value="{{ $field->field_type }}" required>
+            </div>
+            <div class="form-group">
+                <label for="field_size">Field Size</label>
+                <input type="number" class="form-control" id="field_size" name="field_size" value="{{ $field->field_size }}" required>
+            </div>
+            <div class="form-group">
+                <label for="number_of_players">Number of Players</label>
+                <input type="number" class="form-control" id="number_of_players" name="number_of_players" value="{{ $field->number_of_players }}" required>
+            </div>
+            <div class="form-group">
+                <label for="lighting_availability">Lighting Availability</label><br>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="lighting_availability" id="lighting_yes" value="1" {{ $field->lighting_availability == 1 ? 'checked' : '' }}>
+                    <label class="form-check-label" for="lighting_yes">Yes</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="lighting_availability" id="lighting_no" value="0" {{ $field->lighting_availability == 0 ? 'checked' : '' }}>
+                    <label class="form-check-label" for="lighting_no">No</label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
-    @endif
-
-    <form action="{{ route('admin.fields.update', $field->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-
-        <div class="form-group">
-            <label for="field_name">Field Name</label>
-            <input type="text" class="form-control" id="field_name" name="field_name" value="{{ old('field_name', $field->field_name) }}" required>
-        </div>
-        <div class="form-group">
-            <label for="field_location">Location</label>
-            <input type="text" class="form-control" id="field_location" name="field_location" value="{{ old('field_location', $field->field_location) }}" required>
-        </div>
-        <div class="form-group">
-            <label for="field_type">Type</label>
-            <input type="text" class="form-control" id="field_type" name="field_type" value="{{ old('field_type', $field->field_type) }}" required>
-        </div>
-        <div class="form-group">
-            <label for="field_size">Size</label>
-            <input type="number" class="form-control" id="field_size" name="field_size" value="{{ old('field_size', $field->field_size) }}" required>
-        </div>
-        <div class="form-group">
-            <label for="number_of_players">Number of Players</label>
-            <input type="number" class="form-control" id="number_of_players" name="number_of_players" value="{{ old('number_of_players', $field->number_of_players) }}" required>
-        </div>
-        <div class="form-group">
-            <label for="lighting_availability">Lighting Availability</label><br>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" id="lighting_availability" name="lighting_availability" value="1" {{ old('lighting_availability', $field->lighting_availability) ? 'checked' : '' }}>
-                <label class="form-check-label" for="lighting_availability">Yes</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" id="lighting_availability" name="lighting_availability" value="0" {{ !old('lighting_availability', $field->lighting_availability) ? 'checked' : '' }}>
-                <label class="form-check-label" for="lighting_availability">No</label>
-            </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Update Field</button>
-    </form>
-</div>
-@endsection
+</x-app-layout>
