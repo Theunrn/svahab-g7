0e4037d6cea77766da49285a8c3a766c9184fa55
KOEMSRAN.PHON <KOEMSRAN.PHON@PNC.LAN>
2024-06-26 08:47:46 +0700

    [bug] sovle conflict

--- backend/resources/views/setting/user/new.blade.php
+++ backend/resources/views/setting/user/new.blade.php
@@ -1,46 +1,4 @@
 <x-app-layout>
-<<<<<<< HEAD
-   <div>
-        <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
-            <div class="container mx-auto px-6 py-1 pb-16">
-              <div class="bg-white shadow-md rounded my-6 p-5">
-                <form method="POST" action="{{ route('admin.users.store')}}">
-                  @csrf
-                  @method('post')
-                  <div class="flex flex-col space-y-2">
-                    <label for="name" class="text-gray-700 select-none font-medium">User Name</label>
-                    <input id="name" type="text" name="name" value="{{ old('name') }}"
-                      placeholder="Enter name" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                    />
-                </div>
-        
-                <div class="flex flex-col space-y-2">
-                    <label for="email" class="text-gray-700 select-none font-medium">Email</label>
-                    <input id="email" type="text" name="email" value="{{ old('email') }}"
-                      placeholder="Enter email" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                    />
-                </div>
-                
-                <div class="flex flex-col space-y-2">
-                    <label for="phone_number" class="text-gray-700 select-none font-medium">Phone Number</label>
-                    <input id="phone_number" type="text" name="phone_number" value="{{ old('phone_number') }}"
-                      placeholder="Enter Phone Number" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                    />
-                </div>
-                
-                <div class="flex flex-col space-y-2">
-                    <label for="password" class="text-gray-700 select-none font-medium">Password</label>
-                    <input id="password" type="password" name="password" value="{{ old('password') }}"
-                      placeholder="Enter password" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                    />
-                </div>
-                
-                <div class="flex flex-col space-y-2">
-                    <label for="password_confirmation" class="text-gray-700 select-none font-medium">Confirm Password</label>
-                    <input id="password_confirmation" type="password" name="password_confirmation" placeholder="Re-enter password" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                    />
-                </div>
-=======
   <div>
     <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
       <div class="container mx-auto px-6 py-1 pb-16">
@@ -52,12 +10,17 @@
               <label for="name" class="text-gray-700 select-none font-medium">User Name</label>
               <input id="name" type="text" name="name" value="{{ old('name') }}" placeholder="Enter name" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200" />
             </div>
->>>>>>> 68302db391a7306cfa3b10541bd69ede50667dc8
 
             <div class="flex flex-col space-y-2">
               <label for="email" class="text-gray-700 select-none font-medium">Email</label>
               <input id="email" type="text" name="email" value="{{ old('email') }}" placeholder="Enter email" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200" />
             </div>
+            <div class="flex flex-col space-y-2">
+              <label for="phone_number" class="text-gray-700 select-none font-medium">Phone Number</label>
+              <input id="phone_number" type="text" name="phone_number" value="{{ old('phone_number') }}"
+                placeholder="Enter Phone Number" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              />
+          </div>
 
             <div class="flex flex-col space-y-2">
               <label for="password" class="text-gray-700 select-none font-medium">Password</label>
@@ -85,8 +48,6 @@
               <button type="submit" class="bg-blue-500 text-white font-bold px-5 py-1 rounded focus:outline-none shadow hover:bg-blue-500 transition-colors ">Submit</button>
             </div>
         </div>
-
-
       </div>
     </main>
   </div>
