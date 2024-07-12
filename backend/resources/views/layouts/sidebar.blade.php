35438eb12cbfd75b32857642d82e5941182b5968
THEUN.EN <THEUN.EN@PNC.LAN>
2024-07-12 18:24:47 +0700

    [feate] add chat to project #GSA-69

--- backend/resources/views/layouts/sidebar.blade.php
+++ backend/resources/views/layouts/sidebar.blade.php
@@ -25,10 +25,10 @@
         </a>
 
         @canany('User access','User add','User edit','User delete')
-        <a class="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ Route::currentRouteNamed('admin.chats.index') ? 'active' : '' }}"
-            href="{{ route('admin.chats.index')}}">
+        <a class="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100 {{ Route::currentRouteNamed('chatify') ? 'active' : '' }}"
+            href="{{ route('chatify')}}">
             <i class='bx bx-user text-2xl'></i>
-            <span class="mx-3">Chats</span>
+            <span class="mx-3">Chattings</span>
         </a>
         @endcanany
 
