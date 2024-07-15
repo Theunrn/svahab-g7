f817491c30d6b86cf6676bb58aa2e06c2da435e9
THEUN.EN <THEUN.EN@pnc.lan>
2024-07-15 07:40:38 +0700

    [feat] update direct location map

--- frontend/src/Components/FieldDetails/DetailCom.vue
+++ frontend/src/Components/FieldDetails/DetailCom.vue
@@ -3,8 +3,7 @@
   <div class="header-text mt-5">
     <div class="header-detail">
       <div
-        class="select absolute mt-17 bg-green bg-opacity-900 z-20 rounded-lg w-full md:w-5/5 lg:w-9/10 ml-16 flex justify-center items-center"
-      >
+        class="select absolute mt-17 bg-green bg-opacity-900 z-20 rounded-lg w-full md:w-5/5 lg:w-9/10 ml-16 flex justify-center items-center">
         <router-link :to="{path: '/field/detail/' + fieldId, query:{customer:userId}}"  class="menu-item bg-white text-dark border-t-4 border-orange-500">
           <i class="bx bx-home text-2xl"></i>
           <span>Fields</span>
@@ -27,12 +26,7 @@
             <div class="container bg-overlay">
               <div class="flex">
                 <div class="row text-center flex flex-col items-center justify-center h-full">
-                  <button
-                    type="button"
-                    data-bs-toggle="modal"
-                    data-bs-target="#mapModal"
-                    class="btn flex gap-1 btn-primary btn-details py-2 me-2"
-                  >
+                  <button type="button" data-bs-toggle="modal" data-bs-target="#mapModal" class="btn flex gap-1 btn-primary btn-details py-2 me-2" >
                     <i class="bx bxs-map tex-3xl mt-1"></i>
                     <p>Show on map</p>
                   </button>
@@ -78,49 +72,19 @@
             <div class="wrapper-card relative w-full mx-2 my-2 rounded-md">
               <h2>Your Option (optional)</h2>
               <div class="flex items-center">
-                <input
-                  id="vue-checkbox"
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label
-                  for="vue-checkbox"
-                  class="w-full py-2 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >Water</label
-                >
+                <input id="vue-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                <label for="vue-checkbox" class="w-full py-2 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300" >Water</label >
               </div>
               <div class="flex items-center">
-                <input
-                  id="react-checkbox"
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label
-                  for="react-checkbox"
-                  class="w-full py-2 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >Live</label
-                >
+                <input id="react-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                <label for="react-checkbox" class="w-full py-2 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300" >Live</label >
               </div>
               <div class="flex items-center">
-                <input
-                  id="angular-checkbox"
-                  type="checkbox"
-                  value=""
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                />
-                <label
-                  for="angular-checkbox"
-                  class="w-full py-2 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >Take Photo</label
-                >
+                <input id="angular-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                <label for="angular-checkbox" class="w-full py-2 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300" >Take Photo</label  >
               </div>
             </div>
-            <button
-              type="submit"
-              class="btn-book match-btn mr-2 bg-orange-500 w-40 text-white rounded-md px-3 py-1 mt-2 mb-2 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50 shadow-md hover:shadow-lg z-20"
-            >
+            <button type="submit" class="btn-book match-btn mr-2 bg-orange-500 w-40 text-white rounded-md px-3 py-1 mt-2 mb-2 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50 shadow-md hover:shadow-lg z-20" >
               Payment
             </button>
           </form>
@@ -136,21 +100,10 @@
         <div class="gap-2 overflow-y-auto h-50">
           <div class="card-text mt-4">
             <div class="card-display-container gap-3 flex flex-col">
-              <div
-                class="card-display border border-gray-400 rounded-lg shadow-lg flex overflow-hidden"
-                v-for="field in fields"
-                :key="field"
-              >
+              <div class="card-display border border-gray-400 rounded-lg shadow-lg flex overflow-hidden" v-for="field in fields" :key="field" >
                 <div class="relative w-1/3 p-2">
-                  <img
-                    :src="getImageUrl(field.image)"
-                    alt=""
-                    class="w-full h-55 object-cover rounded-md"
-                    style="border-radius: 10px"
-                  />
-                  <span
-                    class="absolute top-5 right-5 bg-gray-200 rounded-full p-1 shadow-md cursor-pointer"
-                  >
+                  <img :src="getImageUrl(field.image)" alt="" class="w-full h-55 object-cover rounded-md" style="border-radius: 10px" />
+                  <span class="absolute top-5 right-5 bg-gray-200 rounded-full p-1 shadow-md cursor-pointer" >
                     <img src="../../assets/image/heart.png" alt="Heart icon" class="w-8 h-8 p-1" />
                   </span>
                 </div>
@@ -167,8 +120,7 @@
                   <div class="text-gray-700">
                     <p class="mt-2 cursor-pointer">
                       <span class="price bg-blue-500 text-white p-2 rounded-md mr-2"
-                        >${{ field.price }}.00</span
-                      >
+                        >${{ field.price }}.00</span >
                     </p>
                     <div class="rating mt-3">
                       <span class="star">&#9733;</span>
@@ -195,16 +147,7 @@
             <div class="flex items-center mb-4">
               <div class="flex-grow flex items-center">
                 <!-- Profile icon -->
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6 text-gray-400 mr-2"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400 mr-2" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" >
                   <circle cx="10" cy="10" r="9" />
                   <path d="M16 16s-1.5-2-4-2-4 2-4 2" />
                   <path d="M9 10s2-3.5 4-3.5 4 3.5 4 3.5" />
@@ -222,17 +165,11 @@
                 }}</label>
               </div>
               <div class="flex justify-end">
-                <button
-                  @click="showEditModal(feedback)"
-                  class="px-2 py-1 bg-blue-500 text-white font-semibold rounded-md shadow hover:bg-blue-600 mr-2"
-                >
+                <button @click="showEditModal(feedback)" class="px-2 py-1 bg-blue-500 text-white font-semibold rounded-md shadow hover:bg-blue-600 mr-2" >
                   Edit
                 </button>
 
-                <button
-                  @click="deleteItem(feedback.id)"
-                  class="px-2 py-1 bg-red-500 text-white font-semibold rounded-md shadow hover:bg-red-600"
-                >
+                <button @click="deleteItem(feedback.id)" class="px-2 py-1 bg-red-500 text-white font-semibold rounded-md shadow hover:bg-red-600" >
                   Delete
                 </button>
               </div>
@@ -243,46 +180,25 @@
     </div>
     <div>
       <h2 class="text-black">Add comment here</h2>
-      <textarea
-        id="comment"
-        name="comment"
-        rows="4"
-        v-model="feedback_text"
-        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-xl text-black"
-        required
-      ></textarea>
+      <textarea id="comment" name="comment" rows="4" v-model="feedback_text" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-xl text-black"  required ></textarea>
     </div>
     <div class="flex justify-end">
-      <button
-        @click="SubmitFeedback"
-        class="px-4 py-2 bg-indigo-500 text-white font-semibold rounded-md shadow hover:bg-indigo-600"
-      >
+      <button @click="SubmitFeedback" class="px-4 py-2 bg-indigo-500 text-white font-semibold rounded-md shadow hover:bg-indigo-600" >
         Submit
       </button>
     </div>
     <!-- Edit Feedback Modal -->
     <div
       v-if="editFeedbackModalVisible"
-      class="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75"
-    >
+      class="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75" >
       <div class="bg-white p-6 rounded-lg w-96">
         <h3 class="text-lg font-semibold mb-4 text-black">Edit Feedback</h3>
-        <textarea
-          v-model="editedFeedbackText"
-          class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
-          rows="4"
-        ></textarea>
+        <textarea v-model="editedFeedbackText" class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black" rows="4" ></textarea>
         <div class="mt-4 flex justify-end">
-          <button
-            @click="updateFeedback()"
-            class="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow hover:bg-blue-600"
-          >
+          <button @click="updateFeedback()" class="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow hover:bg-blue-600" >
             Update
           </button>
-          <button
-            @click="closeEditModal()"
-            class="px-4 py-2 bg-gray-300 text-gray-800 font-semibold rounded-md shadow ml-2 hover:bg-gray-400"
-          >
+          <button @click="closeEditModal()" class="px-4 py-2 bg-gray-300 text-gray-800 font-semibold rounded-md shadow ml-2 hover:bg-gray-400" >
             Cancel
           </button>
         </div>
@@ -290,48 +206,22 @@
     </div>
   </div>
   <!-- map Modal -->
-  <div
-    class="modal fade"
-    id="mapModal"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="mapModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-body">
-          <div
-            class="absolute right-5 bg-gray-200 rounded-full p-2 shadow-md cursor-pointer"
-            style="top: -17px; right: -17px"
-          >
-            <button
-              type="button"
-              class="btn-close text-bold text-2xl bg-gray-200"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
+          <div class="absolute right-5 bg-gray-200 rounded-full p-2 shadow-md cursor-pointer" style="top: -17px; right: -17px" >
+            <button type="button" class="btn-close text-bold text-2xl bg-gray-200" data-bs-dismiss="modal" aria-label="Close" ></button>
           </div>
-          <div class="flex gap-3">
+          <div class="flex gap-4 ">
             <!-- Set height to 50% of the viewport height -->
             <!-- Cards Container -->
-            <div class="flex flex-col w-1/2">
+            <div class="flex flex-col w-1/2 overflow-y-auto overflow-y-scroll">
               <h1 class="mb-2 text-3xl text-red-400">List Fields</h1>
-              <div
-                class="card-display border border-gray-400 rounded-lg shadow-md flex overflow-hidden mb-1"
-                v-for="field in fields"
-                :key="field.id"
-                style="width: 100%"
-              >
+              <div class="card-display border border-gray-400 rounded-lg shadow-md flex overflow-hidden mb-1" v-for="field in fields" :key="field.id" style="width: 100%" >
                 <div class="relative w-full p-2">
-                  <img
-                    :src="getImageUrl(field.image)"
-                    alt=""
-                    class="w-full h-40 object-cover rounded-md"
-                    style="border-radius: 10px"
-                  />
-                  <span
-                    class="absolute top-5 right-5 bg-gray-200 rounded-full p-1 shadow-md cursor-pointer"
-                  >
+                  <img :src="getImageUrl(field.image)" alt="" class="w-full h-40 object-cover rounded-md" style="border-radius: 10px" />
+                  <span class="absolute top-5 right-5 bg-gray-200 rounded-full p-1 shadow-md cursor-pointer" >
                     <img src="../../assets/image/heart.png" alt="Heart icon" class="w-8 h-8 p-1" />
                   </span>
                 </div>
@@ -372,7 +262,8 @@
       </div>
     </div>
   </div>
-  <CurrentUser @address-updated="handleAddressUpdate" />
+  <CurrentUser @location-updated="updateLocation" @address-updated="updateAddress" />
+  <!-- <CurrentUser @address-updated="handleAddressUpdate" /> -->
 </template>
 
 <script setup lang="ts">
@@ -412,8 +303,18 @@ const isclear = ref(false)
 const receivedAddress = ref<string>('')
 
 // Event handler for address-updated event
-const handleAddressUpdate = (address: string) => {
-  receivedAddress.value = address
+// const handleAddressUpdate = (address: string) => {
+//   receivedAddress.value = address
+// }
+
+// Event handler for location-updated event
+const updateLocation = (updatedLocation: string) => {
+  location.value = updatedLocation
+}
+
+// Event handler for address-updated event
+const updateAddress = (updatedAddress: string) => {
+  receivedAddress.value = updatedAddress
 }
 
 const calculateTotalPrice = () => {
