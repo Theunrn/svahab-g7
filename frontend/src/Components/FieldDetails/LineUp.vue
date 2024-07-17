646f901a0e7b7686cedf3ad2e633a075019c36be
KOEMSRAN.PHON <KOEMSRAN.PHON@PNC.LAN>
2024-07-17 10:21:39 +0700

    [imp] update line up

--- frontend/src/Components/FieldDetails/LineUp.vue
+++ frontend/src/Components/FieldDetails/LineUp.vue
@@ -1,92 +1,159 @@
 <template>
   <div class="header-text mt-5">
     <div class="header-detail">
-      <div class="select absolute mt-17 bg-green bg-opacity-900 z-20 rounded-lg w-full md:w-5/5 lg:w-9/10 ml-16 flex justify-center items-center">
-        <router-link :to="{path: '/field/detail/' + fieldId, query:{customer:userId}}"  class="menu-item">
+      <div
+        class="select absolute mt-17 bg-green bg-opacity-900 z-20 rounded-lg w-full md:w-5/5 lg:w-9/10 ml-16 flex justify-center items-center"
+      >
+        <router-link
+          :to="{ path: '/field/detail/' + fieldId, query: { customer: userId } }"
+          class="menu-item"
+        >
           <i class="bx bx-home text-2xl"></i>
           <span>Fields</span>
         </router-link>
-        <router-link :to="{path: '/scheduleField', query:{field:fieldId, user:userId}}" href="#" class="menu-item">
+        <router-link
+          :to="{ path: '/scheduleField', query: { field: fieldId, user: userId } }"
+          href="#"
+          class="menu-item"
+        >
           <i class="bx bx-calendar text-2xl"></i>
           <span>Schedule</span>
         </router-link>
-        <router-link :to="{path: '/lineUp', query:{field:fieldId, user:userId}}" href="#" class="menu-item bg-white text-dark border-t-4 border-orange-500">
+        <router-link
+          :to="{ path: '/lineUp', query: { field: fieldId, user: userId } }"
+          href="#"
+          class="menu-item bg-white text-dark border-t-4 border-orange-500"
+        >
           <i class="bx bx-line-chart text-2xl"></i>
           <span>Line Up</span>
         </router-link>
-        <!-- Select Option Form -->
+        
         <select v-model="selectedOption" class="select-option ml-4 p-2 rounded-lg">
           <option disabled value="">Please select one</option>
           <option value="option1">Option 11 player</option>
           <option value="option2">Option 7 player</option>
         </select>
+        
+        <div class="relative flex gap-5">
+          <input
+            type="text"
+            placeholder="Enter number of players"
+            class="px-4 py-2 rounded-md text-black"
+            v-model="formationInput"
+            @input="formatCardNumber"
+          />
+          <button @click="clearInput" class="absolute right-0 top-0 bg-red-500 text-white px-4 py-2 rounded-md">
+            Clear
+          </button>
+        </div>
       </div>
       <div class="image-container">
-        <img src="../../assets/image/background.jpg" alt="Background Image">
-        <!-- Team Circles -->
-        <div v-for="(position, index) in selectedLineup" :key="index" :class="`circle ${position.team}`" :style="{ top: position.top, left: position.left }"></div>
+        <img src="../../assets/image/background.jpg" alt="Background Image" />
+        <!-- Team A Circles -->
+        <div
+          v-for="(position, index) in formationTeamA"
+          :key="index"
+          :class="`circle team-a`"
+          :style="{ top: position.top, left: position.left }"
+        ></div>
+        <!-- Team B Circles -->
+        <div
+          v-for="(position, index) in formationTeamB"
+          :key="index"
+          :class="`circle team-b`"
+          :style="{ top: position.top, left: position.left }"
+        ></div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
+
 const route = useRoute()
-const userId = computed(() => route.query.user)
-const fieldId = computed(() => route.query.field)
-const selectedOption = ref('');
+const userId = computed(() => route.query.user as string)
+const fieldId = computed(() => route.query.field as string)
 
-const lineups = {
-  option1: [
-    { team: 'team-a', top: '60%', left: '8.4%' },
-    { team: 'team-a', top: '30%', left: '22%' },
-    { team: 'team-a', top: '45%', left: '18%' },
-    { team: 'team-a', top: '76%', left: '18%' },
-    { team: 'team-a', top: '92%', left: '22%' },
-    { team: 'team-a', top: '30%', left: '32%' },
-    { team: 'team-a', top: '60%', left: '32%' },
-    { team: 'team-a', top: '92%', left: '32%' },
-    { team: 'team-a', top: '30%', left: '43%' },
-    { team: 'team-a', top: '60%', left: '43%' },
-    { team: 'team-a', top: '92%', left: '43%' },
-    { team: 'team-b', top: '30%', left: '58%' },
-    { team: 'team-b', top: '60%', left: '58%' },
-    { team: 'team-b', top: '92%', left: '58%' },
-    { team: 'team-b', top: '30%', left: '68%' },
-    { team: 'team-b', top: '60%', left: '68%' },
-    { team: 'team-b', top: '92%', left: '68%' },
-    { team: 'team-b', top: '30%', left: '78%' },
-    { team: 'team-b', top: '45%', left: '83%' },
-    { team: 'team-b', top: '76%', left: '83%' },
-    { team: 'team-b', top: '92%', left: '78%' },
-    { team: 'team-b', top: '60%', left: '93%' },
-  ],
-  option2: [
-    { team: 'team-a', top: '60%', left: '8.4%' },
-    { team: 'team-a', top: '30%', left: '40%' },
-    { team: 'team-a', top: '45%', left: '22%' },
-    { team: 'team-a', top: '60%', left: '32%' },
-    { team: 'team-a', top: '60%', left: '47%' },
-    { team: 'team-a', top: '76%', left: '22%' },
-    { team: 'team-a', top: '92%', left: '40%' },
-    { team: 'team-b', top: '30%', left: '63%' },
-    { team: 'team-b', top: '45%', left: '80%' },
-    { team: 'team-b', top: '60%', left: '70%' },
-    { team: 'team-b', top: '60%', left: '55%' },
-    { team: 'team-b', top: '76%', left: '80%' },
-    { team: 'team-b', top: '92%', left: '63%' },
-    { team: 'team-b', top: '60%', left: '93%' },
-  ],
-  
-};
+const selectedOption = ref('')
+const formationInput = ref('')
+const formationTeamA = ref<{ top: string, left: string }[]>([])
+const formationTeamB = ref<{ top: string, left: string }[]>([])
 
-const selectedLineup = computed(() => lineups[selectedOption.value] || []);
+const updateFormation = () => {
+  const inputValues = formationInput.value.trim().split(' ').map(num => Number(num));
+
+  // Validate input format
+  if (inputValues.some(num => isNaN(num) || num <= 0)) {
+    console.error('Invalid input format');
+    return;
+  }
+
+  // Clear previous formations
+  formationTeamA.value = [];
+  formationTeamB.value = [];
+
+  const totalColumns = inputValues.length;
+  const totalHeight = 100;
+  const centerOffset = 50;
+  const columnGap = 10; // Adjust this value to reduce the gap between columns
+
+  for (let col = 0; col < totalColumns; col++) {
+    const numPlayers = inputValues[col];
+    const verticalGap = totalHeight / (numPlayers + 1);
+
+    for (let i = 0; i < numPlayers; i++) {
+      // Calculate the position relative to the center
+      const topPosition = `${centerOffset + (i - (numPlayers - 1) / 2) * verticalGap + 10}%`;
+
+      // Left side (Team A)
+      formationTeamA.value.push({
+        top: topPosition,
+        left: `${55 - (col + 1) * 8}%`, // Adjust left position based on column number
+      });
+
+      // Right side (Team B)
+      formationTeamB.value.push({
+        top: topPosition,
+        left: `${46 + (col + 1) * 8}%`, // Adjust left position based on column number
+      });
+    }
+  }
+}
+
+
+
+const maxSum = 11
+const formatCardNumber = () => {
+  let cleanedInput = formationInput.value.replace(/\D/g, '');
+
+  let sum = 0;
+  for (let digit of cleanedInput) {
+    sum += parseInt(digit);
+  }
+
+  cleanedInput = cleanedInput.replace(/(\d{1})(?=\d)/g, '$1 ');
+
+  if (sum > maxSum) {
+    let excess = sum - maxSum;
+    let trimmedInput = cleanedInput.split(' ').join('');
+    trimmedInput = trimmedInput.slice(0, -excess);
+
+    formationInput.value = trimmedInput.replace(/(\d{1})(?=\d)/g, '$1 ');
+  } else {
+    formationInput.value = cleanedInput;
+  }
+}
+const clearInput = ()=>{
+  formationInput.value = '';
+}
+
+// Watch for changes in formationInput and update formation accordingly
+watch(formationInput, updateFormation)
 </script>
 
 <style scoped>
-/* Additional styling if needed */
 .header-detail {
   height: 100px;
   background-color: rgb(144, 124, 91);
@@ -94,8 +161,8 @@ const selectedLineup = computed(() => lineups[selectedOption.value] || []);
 
 .select {
   display: flex;
-  justify-content: space-around; /* Centers items horizontally */
-  align-items: center; /* Centers items vertically */
+  justify-content: space-around;
+  align-items: center;
   border-radius: 5px;
   padding: 0.5rem 1rem;
   box-shadow: 0 1px 3px rgba(245, 242, 242, 0.1);
@@ -112,25 +179,25 @@ const selectedLineup = computed(() => lineups[selectedOption.value] || []);
 }
 
 .menu-item:hover {
-  background-color: rgba(255, 255, 255, 0.1); 
+  background-color: rgba(255, 255, 255, 0.1);
 }
 
 .menu-item i {
-  margin-right: 0.5rem; 
+  margin-right: 0.5rem;
 }
 
 .bg-green {
-  background-color: #38a169; /* Tailwind green color */
+  background-color: #38a169;
 }
 
 .bg-opacity-90 {
-  background-color: rgba(56, 161, 105, 0.9); /* Adjusted opacity */
+  background-color: rgba(56, 161, 105, 0.9);
 }
 
 .image-container {
-  width: 98vw; 
-  height: 100vh; 
-  overflow: hidden; 
+  width: 98vw;
+  height: 100vh;
+  overflow: hidden;
   position: relative;
   background-position: center;
 }
@@ -138,7 +205,7 @@ const selectedLineup = computed(() => lineups[selectedOption.value] || []);
 .image-container img {
   min-width: 100%;
   height: 80%;
-  object-fit: cover; 
+  object-fit: cover;
   position: absolute;
   margin: 10px;
   top: 19%;
@@ -154,14 +221,13 @@ const selectedLineup = computed(() => lineups[selectedOption.value] || []);
 }
 
 .team-a {
-  background-color: rgba(221, 51, 51, 0.932); /* Red color with opacity */
+  background-color: rgba(221, 51, 51, 0.932);
 }
 
-.team-b{
-  background-color: rgba(45, 45, 211, 0.938); /* Blue color with opacity */
+.team-b {
+  background-color: rgba(45, 45, 211, 0.938);
 }
 
-/* Style for the select option */
 .select-option {
   background-color: white;
   color: black;
--- frontend/src/Components/Layouts/WebLayout.vue
+++ frontend/src/Components/Layouts/WebLayout.vue
@@ -43,18 +43,13 @@
           </div>
 
           <div class="relative flex gap-10 w-[334px]">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-              </svg>
-            </div>
-            <VueFlatpickr v-model="dateRange" :config="flatpickrConfig" class="px-4 py-3 text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
+            <div class="px-4 py-3 rounded-none rounded-s-lg bg-gray-50 border text-gray-900 leading-none focus:ring-blue-500 focus:border-blue-500 block flex-1 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" min="09:00" max="18:00" value="00:00" ><strong>OPEN DAY : </strong> MONDAY to SUNDAY </div>
           </div>
 
           <form class="w-[334px]">
             <div class="flex">
-              <!-- Time input -->
-              <input type="time" id="time" class="px-4 py-3 rounded-none rounded-s-lg bg-gray-50 border text-gray-900 leading-none focus:ring-blue-500 focus:border-blue-500 block flex-1 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" min="09:00" max="18:00" value="00:00" required />
+              
+              <div class="px-4 py-3 rounded-none rounded-s-lg bg-gray-50 border text-gray-900 leading-none focus:ring-blue-500 focus:border-blue-500 block flex-1 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" min="09:00" max="18:00" value="00:00" ><strong>OPEN TIME : </strong> 7:00 AM to 10:00 PM </div>
             </div>
           </form>
         </div>
